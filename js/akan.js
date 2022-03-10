c101d83b6d3f9d932d9d71c0c7fe9642b54fb0ab
chegeian <chegeian65@gmail.com>
2022-03-10 11:05:44 +0300

    edited each file

--- js/akan.js
+++ js/akan.js
@@ -1,37 +1,55 @@
-function generateName(){
-	let date = document.getElementById("date").value;
+function readData() {
+	var dd = document.getElementById("dd").value; 
+	var mm = document.getElementById("mm").value;  
+	var yy = document.getElementById("yy").value; 
+	var male = document.getElementById("male").checked; 
+	var female = document.getElementById("female").checked; 
+	var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
+	var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]; 
+	var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+	var birthDate = new Date(mm + '/' + dd + '/' + yy);
+	var dayOfTheWeek = birthDate.getDay(); 
 
-	let month = document.getElementById("month").value;
-
-	let year = document.getElementById("year").value;
-
-	let gender = document.getElementById("gender").value;
-
-	let century = year.slice(0,2);
-
-	let yearName = year.slice(2,4);
-
-	let day = ( ( (century/4) -2*yearName-1) + ((5*yearName/4) ) + ((26*(month+1)/10)) + date ) % 7;
-	
-	day = Math.floor(day)
-	
-	var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-
-	var femaleNames = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
-
-	if (gender === "male"){
-			document.getElementById("akan").innerHTML = maleNames[day - 1]
-	}
-	else{
-			document.getElementById("akan").innerHTML= femaleNames[day - 1]
-	}
-	if(date <= 0 || date > 31){
-			alert("The date entered is invalid")
-	}
-	else if(month <= 0 || month > 12){
-			alert("The month entered is invalid")
-	}
-	else if(year < 1000 || year >= 9999){
-			alert("The year entered is invalid")
+	if (dd <= 0 || dd > 31) { 
+		document.getElementById("error").innerText = "Please enter a valid date!" 
+		document.getElementById("error").style.color = "red"
+		document.getElementById("error").style.paddingLeft = "36%"
+	} else if ((mm === "September" || mm === "April" || mm === "June" || mm === "November") && (dd <= 0 || dd > 30)) { 
+		document.getElementById("error").innerText = "Please enter a valid date!" /r 
+		document.getElementById("error").style.color = "red"
+		document.getElementById("error").style.paddingLeft = "36%"
+	} else if (mm === "------") { 
+		document.getElementById("error").innerText = "Please select a month!" 
+		document.getElementById("error").style.color = "red"
+		document.getElementById("error").style.paddingLeft = "38%"/
+		document.getElementById("error").innerText = "Please enter a valid year!" /
+		document.getElementById("error").style.color = "red"
+		document.getElementById("error").style.paddingLeft = "36%"
+	} else if ((mm === "February") && (dd <= 0 || dd > 29) && (0 == yy % 4)) { 
+		document.getElementById("error").innerText = "Please enter a valid date!" 
+		document.getElementById("error").style.color = "red"
+		document.getElementById("error").style.paddingLeft = "36%"
+	} else if ((mm === "February") && (dd <= 0 || dd > 28) && (0 != yy % 4)) { 
+		document.getElementById("error").innerText = "Please enter a valid date!" /
+		document.getElementById("error").style.color = "red"
+		document.getElementById("error").style.paddingLeft = "36%"
+	} else if (male === true) { 
+		document.getElementById("result").innerHTML = "You were born on a " + days[dayOfTheWeek] + ".\n" + "Your Akan name is " + maleNames[dayOfTheWeek] + "!" //shows result by replacing the  HTML content in the id=result
+		document.getElementById("result").style.color = "MediumBlue"
+		document.getElementById("result").style.padding = "10% 12% 10% 12%"
+		document.getElementById("result").style.fontSize = "35px"
+		document.getElementById("result").style.fontFamily = "'Kalam', cursive"
+		document.getElementById("result").style.textAlign = "center"
+	} else if (female === true) { 
+		document.getElementById("result").innerHTML = "You were born on a " + days[dayOfTheWeek] + ".\n" + "Your Akan name is " + femaleNames[dayOfTheWeek] + "!" //shows result by replacing the HTML content in the id=result
+		document.getElementById("result").style.color = "Deeppink"
+		document.getElementById("result").style.padding = "10% 12% 10% 12%"
+		document.getElementById("result").style.fontSize = "35px"
+		document.getElementById("result").style.fontFamily = "'Kalam', cursive"
+		document.getElementById("result").style.textAlign = "center"
+	} else if ((female === false) && (male === false)) { /
+		document.getElementById("error").innerText = "Please select gender!" 
+		document.getElementById("error").style.color = "red"
+		document.getElementById("error").style.paddingLeft = "38%"
 	}
 }
